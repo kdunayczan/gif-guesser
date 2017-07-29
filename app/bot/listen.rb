34e07ec2d1599e0cccf779fd0371aacc840d670f
Katie Dunayczan <katie.dunayczan@gmail.com>
2017-07-29 10:25:14 -0400

    categories and play again

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -14,10 +14,11 @@ Bot.on :message do |message|
 		    type: 'template',
 		    payload: {
 		      template_type: 'button',
-		      text: 'Would you like to play?',
+		      text: 'Pick a category:',
 		      buttons: [
-		        { type: 'postback', title: 'Yes', payload: 'PLAY' },
-		        { type: 'postback', title: 'No', payload: 'QUIT' }
+		        { type: 'postback', title: 'Actions', payload: 'ACTIONS' },
+		        { type: 'postback', title: 'Animals', payload: 'ANIMALS' },
+		        { type: 'postback', title: 'Sports', payload: 'SPORTS' }
 		      ]
 		    }
 		  }
@@ -25,15 +26,31 @@ Bot.on :message do |message|
 
 	elsif is_correct?(message)
 		message.reply(text: 'You are correct!')
+		
+		message.reply(
+		  attachment: {
+		    type: 'template',
+		    payload: {
+		      template_type: 'button',
+		      text: 'Pick a category:',
+		      buttons: [
+		        { type: 'postback', title: 'Actions', payload: 'ACTIONS' },
+		        { type: 'postback', title: 'Animals', payload: 'ANIMALS' },
+		        { type: 'postback', title: 'Sports', payload: 'SPORTS' }
+		      ]
+		    }
+		  }
+		)
 	else
-		message.reply(text: 'Nope!')
+		message.reply(text: 'Nope! Try Again.')
 	end	 
 end
 
 Bot.on :postback do |postback|
 	case postback.payload
-	when 'PLAY'
-  	generate_random_gif
+	when 'ACTIONS'
+  	data = CSV.read("actions.csv")
+		@random_gif = data[0].sample
 		url = "http://api.giphy.com/v1/gifs/search?q=#{@random_gif}&api_key=#{ENV['GIPHY_API_KEY']}"
 		resp = Net::HTTP.get_response(URI.parse(url))
 		buffer = resp.body
@@ -53,16 +70,51 @@ Bot.on :postback do |postback|
 		postback.reply(
 			text: "What is your guess?"
 		)
-	when 'QUIT'
+	when 'ANIMALS'
+  	data = CSV.read("animals.csv")
+		@random_gif = data[0].sample
+		url = "http://api.giphy.com/v1/gifs/search?q=#{@random_gif}&api_key=#{ENV['GIPHY_API_KEY']}"
+		resp = Net::HTTP.get_response(URI.parse(url))
+		buffer = resp.body
+		result = JSON.parse(buffer)
+
+	  attachment = { 
+	  	type: 'image', 
+	  	payload: { 
+	  		url: result["data"].sample["images"]["original"]["url"] 
+	  	} 
+	  }
+
 		postback.reply(
-			text: "Goodbye!"
+			attachment: attachment
+		)
+
+		postback.reply(
+			text: "What is your guess?"
+		)
+	when 'SPORTS'
+  	data = CSV.read("sports.csv")
+		@random_gif = data[0].sample
+		url = "http://api.giphy.com/v1/gifs/search?q=#{@random_gif}&api_key=#{ENV['GIPHY_API_KEY']}"
+		resp = Net::HTTP.get_response(URI.parse(url))
+		buffer = resp.body
+		result = JSON.parse(buffer)
+
+	  attachment = { 
+	  	type: 'image', 
+	  	payload: { 
+	  		url: result["data"].sample["images"]["original"]["url"] 
+	  	} 
+	  }
+
+		postback.reply(
+			attachment: attachment
+		)
+
+		postback.reply(
+			text: "What is your guess?"
 		)
 	end
-end
-
-def generate_random_gif 
-	data = CSV.read("tags.csv")
-	@random_gif = data[0].sample
 end
 
 def is_correct?(message)
