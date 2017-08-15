7233cab2e8e39e4bcb5347823290d6ce6190ad7d
Katie Dunayczan <katie.dunayczan@gmail.com>
2017-08-15 14:14:58 -0400

    refactored single player gameplay

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -7,36 +7,46 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
+@categories = ["Actions", "Food & Drink", "Animals", "TV Shows", "Sports", "Music"]
+@position = 0
+@how_many = 3
+
 Bot.on :message do |message|
-
-	if message.text.downcase == 'hi' || message.text.downcase == 'back'
-		text = 'Pick a category or type "more" to see more categories:'
-		title_1 = "Actions"
-		title_2 = "Food & Drink"
-		title_3 = "Animals"
-		message.reply(attachment: load_categories(text, title_1, title_2, title_3))
-
-	elsif message.text.downcase == 'more'
-		text = 'Pick a category or type "back" to go back:'
-		title_1 = "TV Shows"
-		title_2 = "Sports"
-		title_3 = "Music"
-		message.reply(attachment: load_categories(text, title_1, title_2, title_3))
-
-	elsif is_correct?(message)
-		message.reply(text: 'You are correct!')
-
-		text = 'Pick a category or type "more" to see more categories:'
-		title_1 = "Actions"
-		title_2 = "Food & Drink"
-		title_3 = "Animals"
-		message.reply(attachment: load_categories(text, title_1, title_2, title_3))
-
-	elsif message.text.downcase == 'give up'
+	response = message.text.downcase
+	if @random_gif 
+		case response
+		when @random_gif
+			message.reply(text: "You are correct!\n'start' to play again.")
+			@random_gif = nil
+		when "give up"
 			message.reply(text: "Correct answer: #{@random_gif}")
+			@random_gif = nil
+		else
+			message.reply(text: "Nope! Try Again!\n'give up' to see the answer")
+		end
 	else
-		message.reply(text: "Nope! Try Again!\nOr type 'give up' to see the answer")
-	end	 
+		case response
+		when "start" 
+			@position = 0
+			@text = "Pick a category!\n'more' to see more categories"
+			message.reply(get_categories)		
+		when "more"
+			@position += @how_many
+			@text = "Pick a category!\n'back' to go back\n'more' to see more categories"
+			message.reply(get_categories)		
+		when "back"
+			if @position > 2
+				@position -= @how_many
+				@text = "Pick a category!\n'back' to go back\n'more' to see more categories"
+				message.reply(get_categories)
+			else
+				@text = "Pick a category!\n'more' to see more categories"
+				message.reply(get_categories)
+			end
+		else
+			message.reply(text: "Welcome to Gif Guesser! Please type 'start' to begin.")
+		end
+	end
 end
 
 Bot.on :postback do |postback|
@@ -47,10 +57,6 @@ Bot.on :postback do |postback|
 	postback.reply(
 		text: "What is your guess?"
 	)
-end
-
-def is_correct?(message)
-	message.text.downcase == @random_gif
 end
 
 def gif_request(category)
@@ -69,21 +75,27 @@ def gif_request(category)
   }
 end
 
-def load_categories(text, title_1, title_2, title_3)
+def get_categories
+	@options = @categories[@position, @how_many]
+	return { 
+		attachment: load_categories(@text, @options) 
+	}
+end
+
+def load_categories(text, options)
 	return {
     type: 'template',
     payload: {
       template_type: 'button',
       text: "#{text}",
       buttons: [
-        { type: 'postback', title: "#{title_1}", payload: "#{title_1.upcase.delete(' ')}" },
-        { type: 'postback', title: "#{title_2}", payload: "#{title_2.upcase.delete(' ')}" },
-        { type: 'postback', title: "#{title_3}", payload: "#{title_3.upcase.delete(' ')}" }
+        { type: 'postback', title: "#{options[0]}", payload: "#{options[0].upcase.delete(' ')}" },
+        { type: 'postback', title: "#{options[1]}", payload: "#{options[1].upcase.delete(' ')}" },
+        { type: 'postback', title: "#{options[2]}", payload: "#{options[2].upcase.delete(' ')}" }
       ]
     }
   }
 end
-
-
+ 
 
 
